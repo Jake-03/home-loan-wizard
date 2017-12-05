19ee2390372e2a7f60bd2c9fb4825adb4e4c35db
JACOB MORRISON <jacobmorrison@JACOBs-MacBook-Pro.local>
2017-12-05 10:37:12 -0700

    hqg2-selectors

--- nightwatch/tests/example_test.js
+++ nightwatch/tests/example_test.js
@@ -9,19 +9,34 @@ module.exports = {
     after: browser => {
         browser.end()
     },
-    'I can put data in the first screen' : (browser) => {
+    // 'I can put data in the first screen' : (browser) => {
+    //     browser
+    //         .click(selectors.homeScreen.startButton)
+    //         .waitForElementNotPresent(selectors.homeScreen.startButton, 1000)
+    //         .waitForElementVisible(selectors.loanPropTypeScreen.loanDropDown, 1000)
+    //     //click the right option in the loantype dropdown, then the option selectors, with the field I want chosen (from my test data)
+    //     functions.clickOption(selectors.loanPropTypeScreen.loanDropDown, selectors.loanPropTypeScreen.loanSelectors[data.transactions.firstEntry.loanType], browser)
+    //     //click the right option in the proptype dropdown, then the option selectors, with the field I want chosen (from my test data)
+    //     functions.clickOption(selectors.loanPropTypeScreen.propDropDown, selectors.loanPropTypeScreen.propSelectors[data.transactions.firstEntry.propType], browser)
+
+    //     //now leave the screen
+    //     browser
+    //         .click(selectors.loanPropTypeScreen.nextButton)
+    //         .waitForElementNotPresent(selectors.loanPropTypeScreen.loanDropDown, 1000)
+    'Test good data': (browser) => {
         browser
             .click(selectors.homeScreen.startButton)
-            .waitForElementNotPresent(selectors.homeScreen.startButton, 1000)
             .waitForElementVisible(selectors.loanPropTypeScreen.loanDropDown, 1000)
-        //click the right option in the loantype dropdown, then the option selectors, with the field I want chosen (from my test data)
-        functions.clickOption(selectors.loanPropTypeScreen.loanDropDown, selectors.loanPropTypeScreen.loanSelectors[data.transactions.firstEntry.loanType], browser)
-        //click the right option in the proptype dropdown, then the option selectors, with the field I want chosen (from my test data)
-        functions.clickOption(selectors.loanPropTypeScreen.propDropDown, selectors.loanPropTypeScreen.propSelectors[data.transactions.firstEntry.propType], browser)
-        
-        //now leave the screen
-        browser
-            .click(selectors.loanPropTypeScreen.nextButton)
-            .waitForElementNotPresent(selectors.loanPropTypeScreen.loanDropDown, 1000)
-    }
+            // click loan type dropdown and choose refinance, click in type of property dropdown and choose town home
+            .click(`${selectors.loanPropTypeScreen.loanSelectors.optionRefi}`)
+            .click(`${selectors.loanPropTypeScreen.propSelectors.optionTown}`)
+            .click(`${selectors.cityLocationScreen.nextButton}`)
+
+        // City Location Screen. Enter city name. 
+            .waitForElementVisible(selectors.cityLocationScreen.cityInput, 1000)
+            functions.input(selectors.cityLocationScreen.cityInput("Salt Lake City"), browser)
+            .click(selectors.cityLocationScreen.nextButton)
+    },
+
+
 }
