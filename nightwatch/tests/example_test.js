70cb9efb21c7e5bc48b7507a663b193201e10c5e
Adam Hair <AdamHair@Dimitri-2.local>
2017-12-06 12:05:44 -0700

    test suite

--- nightwatch/tests/example_test.js
+++ nightwatch/tests/example_test.js
@@ -88,9 +88,10 @@ module.exports = {
         browser.expect.element(selectors.overviewScreenOutput.currentAddress).text.to.contain('4716 Kemper Lane Salt Lake City 84104')
             browser
             .click(selectors.overviewScreen.sendButton)
+            .click(selectors.thankYouScreen.homeButton)
     },
     
-    'Test bad Data': (browser) => {
+    'Test bad Data "send"': (browser) => {
         browser
             .click(selectors.homeScreen.startButton)
             .waitForElementVisible(selectors.loanPropTypeScreen.loanDropDown, 1000)
@@ -154,5 +155,71 @@ module.exports = {
         browser.expect.element(selectors.overviewScreenOutput.currentAddress).text.to.contain('thisisaninvalidaddress')
         browser
             .click(selectors.overviewScreen.sendButton)
-    }
-}
+    },
+            
+        'Test bad Data "start again"': (browser) => {
+                browser
+                    .click(selectors.homeScreen.startButton)
+                    .waitForElementVisible(selectors.loanPropTypeScreen.loanDropDown, 1000)
+                    //click in loan type dropdown and choose refinance, click in type of property dropdown and choose townhome
+                    .click(`${selectors.loanPropTypeScreen.loanSelectors.optionEquity}`)
+                    .click(`${selectors.loanPropTypeScreen.propSelectors.optionMulti}`)
+                    .click(selectors.loanPropTypeScreen.nextButton)
+                    // City Location Screen. Enter city name
+                    .waitForElementVisible(selectors.cityPropLocatedScreen.cityName, 1000)
+                functions.input(selectors.cityPropLocatedScreen.cityName, data.badData.cityPropLocatedScreen.inputCityName, browser)
+                browser
+                    .click(selectors.cityPropLocatedScreen.wTwoButton)
+                    // Property Loan Screen, Click on loan use
+                    .waitForElementVisible(selectors.whatPropLoanScreen.whatPropText, 1000)
+                    .click(selectors.whatPropLoanScreen.secondary)
+                    // Found new home screen
+                    .waitForElementVisible(selectors.foundNewHomeScreen.foundHomeText, 1000)
+                    .click(selectors.foundNewHomeScreen.noFound)
+                    // Real estate agent screen
+                    .waitForElementVisible(selectors.realEstateAgentScreen.realEstateText, 1000)
+                    .click(selectors.realEstateAgentScreen.noAgent)
+                    // Purchase price/down payment screen
+                    .waitForElementVisible(selectors.priceDownPaymentScreen.downPaymentText, 1000)
+                functions.input(selectors.priceDownPaymentScreen.purchasePrice, data.badData.priceDownPaymentScreen.inputEstimatedPrice, browser)
+                functions.input(selectors.priceDownPaymentScreen.downPayment, data.badData.priceDownPaymentScreen.inputDownPayment, browser)
+                browser
+                    .click(selectors.priceDownPaymentScreen.wSixButton)
+                    // Estimate credit score
+                    .waitForElementVisible(selectors.creditScoreScreen.creditScoreText, 1000)
+                    .click(selectors.creditScoreScreen.fair)
+                    // Forclosure screen
+                    .waitForElementVisible(selectors.bankForeScreen.bankForeText, 1000)
+                    .click(selectors.bankForeScreen.both)
+                    // Address screen
+                    .waitForElementVisible(selectors.addressScreen.addressText, 1000)
+                functions.input(selectors.addressScreen.addressOne, data.badData.addressScreen.inputAddressLineOne, browser)
+                functions.input(selectors.addressScreen.addressTwo, data.badData.addressScreen.inputAddressLineTwo, browser)
+                functions.input(selectors.addressScreen.addressThree, data.badData.addressScreen.inputAddressLineThree, browser)
+                browser
+                    .click(selectors.addressScreen.marginButton)
+                // Name screen
+                    .waitForElementVisible(selectors.nameScreen.nameText, 1000)
+                functions.input(selectors.nameScreen.firstName, data.badData.nameScreen.inputNameLineOne, browser)
+                functions.input(selectors.nameScreen.lastName, data.badData.nameScreen.inputLastLineTwo, browser)
+                functions.input(selectors.nameScreen.email, data.badData.nameScreen.inputEmailLineThree, browser)
+                browser
+                    .click(selectors.nameScreen.marginButton)
+                    // Overview
+                browser.expect.element(selectors.overviewScreenOutput.nameOutput).text.to.equal('12345 123notlastname')
+                browser.expect.element(selectors.overviewScreenOutput.emailOutput).text.to.equal('invalidemail')
+                browser.expect.element(selectors.overviewScreenOutput.typeOfLoan).text.to.equal('Home Equity')
+                browser.expect.element(selectors.overviewScreenOutput.propertyType).text.to.equal('Multi Family Dwelling')
+                browser.expect.element(selectors.overviewScreenOutput.city).text.to.equal('asdfasdf')
+                browser.expect.element(selectors.overviewScreenOutput.loanPropertyType).text.to.equal('Secondary Home')
+                browser.expect.element(selectors.overviewScreenOutput.foundNewHome).text.to.equal('False')
+                browser.expect.element(selectors.overviewScreenOutput.realEstateAgent).text.to.equal('False')
+                browser.expect.element(selectors.overviewScreenOutput.purchasePrice).text.to.equal('invalidinformation')
+                browser.expect.element(selectors.overviewScreenOutput.downPayment).text.to.equal('invalidinformation')
+                browser.expect.element(selectors.overviewScreenOutput.creditScore).text.to.equal('Fair')
+                browser.expect.element(selectors.overviewScreenOutput.bankruptcyHistory).text.to.equal('Has had both a foreclosure and a bankruptcy')
+                browser.expect.element(selectors.overviewScreenOutput.currentAddress).text.to.contain('thisisaninvalidaddress')
+                browser
+                    .click(selectors.overviewScreen.startOver)
+            }
+        }
