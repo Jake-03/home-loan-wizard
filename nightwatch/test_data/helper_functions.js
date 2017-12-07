2be472494cf3351e0a81aa77dde25017654d7961
Adam Hair <AdamHair@Dimitri-2.local>
2017-12-07 09:04:00 -0700

    full test suite

--- nightwatch/test_data/helper_functions.js
+++ nightwatch/test_data/helper_functions.js
@@ -1,8 +1,14 @@
-const input = (selector, value, browser) => {
+// Types in value into input found by selector.
+// Expects the input's text to match expected value.
+// If expected value is not supplied value is used instead.
+const input = (selector, value, browser, expectedValue) => {
+    if (expectedValue === undefined) {
+        expectedValue = value
+    }
     browser
         .clearValue(selector)
         .setValue(selector, value)
-        .expect.element(selector).to.have.value.that.equals(value)
+        .expect.element(selector).to.have.value.that.equals(expectedValue)
 }
 const clickOption = (dropdownSelector, optionSelector, browser) => {
     browser
